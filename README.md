a268f61019d9d97965c842d494a12625811bb17b
yilingsj <315800015@qq.com>
2020-04-15 21:00:04 +0800

    [add] 新增一个兼容苹果手机的底部评论框组件

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # weixinxiaochengxu
 微信小程序-日常开发爬坑记录
 
-由于每个demo都是独立的组件，所以master分支及dev分支不合并代码。每次新提交的demo会以“dev-[name]-[time]”这样的格式区分开来，看官需要下载哪个demo就去对应的分支拉代码，这样做的好处就是：每个分支的代码独立且互不依赖。
+## 新增一个兼容ios手机的底部评论框
+分支：dev-textarea-fixed-bottom-20200410
 
-每个分支中的README.md文件会用简单的话描述对应的demo是做什么的，同时也会放相应的视频用于演示问题。
+说明：固定在底部的评论框一直是前端项目中的一个魔鬼，总是会有那么一种情况输入框被系统键盘遮挡，或者是输入框悬浮于半空中但键盘没有展开。写这个分支时，艺灵也是经过了各种折腾，wxss的样式也会引起一些莫名的bug！wx:if或wx:for也不建议使用，因为在某些低版本微信中，在首次点击时也会存在bug！废话不多说，上最终的视频
 
-dev分支中的README.md 相当于一个总的列表，暂无他用，纯粹是为了显示目前已经有哪些分支罢了。
+视频演示：
+兼容苹果手机的底部评论框
+
+[![兼容苹果手机的底部评论框](https://img.alicdn.com/imgextra/i1/6000000004562/O1CN01ouaYAe1jZRRdgxmPk_!!6000000004562-0-tbvideo.jpg)](https://cloud.video.taobao.com//play/u/759415648/p/1/e/6/t/1/259290038850.mp4)
+
+踩坑文章地址：
+(微信小程序从入坑到放弃二十二：完美兼容安卓和ios手机的底部评论框)[http://www.yilingsj.com/xwzj/2020-04-10/weixin-textarea-fixed-bottom.html]
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,14 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    popupEvaluation: {
+      isShow: false, // 评论框弹层弹窗是否显示
+      id: null, // id
+      typeName: '', // 类型：商城 / 资讯 / 名片
+      isClear: false // 是否清空上次的数据
+    },
+    evaluationData: [] // 评论数据
   },
   //事件处理函数
   bindViewTap: function() {
@@ -49,6 +56,71 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  /**
+   * @author: adu（duhw@5ifengdu.com）
+   * @description: 评论数据
+   * @param {Object} event 元素本身
+   * @return {void}
+   * @Date: 2020-04-10 15:24:45
+   */
+  myeventEvaluationData(event) {
+    let { isShow, typeName, content, id } = event.detail
+    if (typeName) { // 点击了发布评论按钮
+      wx.showLoading({
+        title: '发布中...'
+      })
+      switch (typeName) {
+        case 'infomation': // 资讯
+          console.log('资讯id=', id, ';评论内容=', content)
+          break
+        case 'mall': // 商城
+          console.log('商品id=', id, ';评论内容=', content)
+          break
+        case 'card': // 名片的评论
+          console.log('名片的评论id=', id, ';评论内容=', content)
+          break
+        default:
+          break
+      }
+      // 此处的延迟是模拟发送数据，真实场景应在上面的switch中进行
+      setTimeout(() => {
+        const index = this.data.evaluationData.length - 1 >= 0 ? this.data.evaluationData.length : 0
+        let editEvaluationData = 'evaluationData[' + index + ']'
+        const data = { text: content, type: typeName, time: Date.now() }
+        this.setData({
+          'popupEvaluation.isShow': false,
+          'popupEvaluation.isClear': true,
+          [editEvaluationData]: data
+        })
+        wx.showLoading({
+          title: '发布成功',
+        })
+        setTimeout(() => {
+          wx.hideLoading()
+        }, 500)
+      }, 1000)
+    } else {
+      console.log('普通关闭')
+      this.setData({
+        'popupEvaluation.isShow': isShow,
+        'popupEvaluation.isClear': false
+      })
+    }
+  },
+  /**
+   * @author: yilingsj（315800015@qq.com）
+   * @description: 点击时显示评论组件
+   * @return {void}
+   * @Date: 2020-04-10 21:59:38
+   */
+  handleIsShowComment() {
+    this.setData({
+      'popupEvaluation.isShow': true,
+      'popupEvaluation.id': 0, /* id为动态，请修改 */
+      'popupEvaluation.typeName': 'card',  /* typeName也为动态，请修改 */
+      'popupEvaluation.isClear': this.data.popupEvaluation.isClear
     })
   }
 })
