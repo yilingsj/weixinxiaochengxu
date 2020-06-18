0c137c4fa432b87c82caa06027be5ba05d35d628
yilingsj <315800015@qq.com>
2020-06-19 00:18:02 +0800

    [add] 新增demo: wx.pageScrollTo实现页面滚动到指定位置

--- README.md
+++ README.md
@@ -1,38 +1,12 @@
 # weixinxiaochengxu
 微信小程序-日常开发爬坑记录
 
-## 新增demo：scroll-view组件实现评论前后页面滚动到指定评论处的功能
-分支：dev-scroll-view-scrollIntoView-20200618
-
-## 新增demo：wx.pageScrollTo实现页面滚动到指定位置
+新增demo：wx.pageScrollTo实现页面滚动到指定位置
 分支：dev-wx-pageScrollTo-20200609
 
-## 新增demo：冒泡事件bindtap和catchtap的区别
-分支：dev-bindtap-and-catchtap-20200605
+需求： 用户点击评论框时，最后一条评论滚动到评论框的上方；当用户发表评论后，最新评论出现在页面顶部。
 
-## 新增demo: 开发一款DIY音乐播放器(WXS优化版)
-分支：dev-wxs-diyAudioPlayer-20200512
+[视频](https://cloud.video.taobao.com//play/u/759415648/p/1/e/6/t/1/268522835395.mp4)
 
-## 新增一个可控制播放进度的音乐播放器(初级版)
-分支：dev-diyAudioPlayer-20200510
-
-## 新增一个在WXS中使用split实现金额小数点前的数比后面的大的demo
-分支：dev-wxs-split-20200428
-
-## 新增一个在wxs中使用正则表达式的demo
-分支：dev-wxs-regexp-20200427 
-
-## 新增一个3种方法实现长按保存图片到相册的demo
-分支：dev-longpress-to-save-the-picture-20200424
-
-## 新增一个textarea在ios上存在bug的demo
-分支：dev-textarea-padding-bug-in-ios-20200414 
-
-## 新增一个兼容ios手机的底部评论框
-分支：dev-textarea-fixed-bottom-20200410
-
-## 新增一个使用wx.saveImageToPhotosAlbum保存图片到相册时检测权限的demo
-分支：dev-scope-writePhotosAlbum-20200406
-
-## 新增一个wx.downloadFile下载图片的demo
-分支：dev-downloadFile-20200404
+踩坑文章地址： 
+[微信小程序从入坑到放弃三十：wx.pageScrollTo在评论功能中的应用](http://www.yilingsj.com/xwzj/2020-06-18/weixin-scroll-into-view.html)
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,185 @@
+/*
+ * @Description: 
+ * @Author: yilingsj（315800015@qq.com）
+ * @Date: 2020-06-09 11:20:33
+ * @LastEditors: yilingsj（315800015@qq.com）
+ * @LastEditTime: 2020-06-09 18:06:03
+ */
 //index.js
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    popupEvaluation: {
+      isShow: false, // 评论框弹层弹窗是否显示
+      id: null, // id
+      typeName: '', // 类型：商城 / 资讯 / 名片
+      isClear: false // 是否清空上次的数据
+    },
+    evaluationData: [], // 评论数据
+    footerDefaultHeight: ''
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
+    this.initData() // 未对接接口时，初始化一堆数据便于观察
+    this.getDom('.page-footer', res => {
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        footerDefaultHeight: res[0].height // 底部默认高度
+      });
+    });
+  },
+  /**
+   * @author: adu（duhw@5ifengdu.com）
+   * @description: 评论数据
+   * @param {Object} event 元素本身
+   * @return {void}
+   * @Date: 2020-04-10 15:24:45
+   */
+  myeventEvaluationData(event) {
+    let {
+      isShow,
+      typeName,
+      content,
+      id,
+      keyHeight,
+      domHeight
+    } = event.detail
+    if (typeName) { // 点击了发布评论按钮
+      wx.showLoading({
+        title: '发布中...'
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+      switch (typeName) {
+        case 'infomation': // 资讯
+          console.log('资讯id=', id, ';评论内容=', content)
+          break
+        case 'mall': // 商城
+          console.log('商品id=', id, ';评论内容=', content)
+          break
+        case 'card': // 名片的评论
+          console.log('名片的评论id=', id, ';评论内容=', content)
+          break
+        default:
+          break
       }
+      // 此处的延迟是模拟发送数据，真实场景应在上面的switch中进行
+      setTimeout(() => {
+        let editEvaluationData = this.data.evaluationData
+        const data = {
+          name: '最新评论' + (this.data.evaluationData.length + 1),
+          text: content,
+          type: typeName,
+          time: Date.now()
+        }
+        editEvaluationData.splice(0, 0, data)
+        this.setData({
+          'popupEvaluation.isShow': false,
+          'popupEvaluation.isClear': true,
+          evaluationData: editEvaluationData,
+          paddingHeight: ''
+        });
+        this.pageScrollFirst(); /* 新增：滚动到最新评论处 */
+        wx.showLoading({
+          title: '发布成功',
+        })
+        this.pageScrollFirst() // 滚动到最新评论处
+        setTimeout(() => {
+          wx.hideLoading()
+        }, 500)
+      }, 1000)
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+      console.log('普通关闭')
+      if (keyHeight) { // 软键盘弹起时
+        this.setData({
+          paddingHeight: domHeight / 2 + keyHeight + this.data.footerDefaultHeight
+        })
+        if (!wx.canIUse('pageScrollTo.object.selector')) {
+          wx.showToast({
+            title: '当前微信版本过低，不支持selector',
+            icon: 'none',
+            duration: 2000
           })
         }
-      })
+        this.pageScrollBottom() // 滚动到最后一条评论
+      } else {
+        this.setData({
+          'popupEvaluation.isShow': isShow,
+          'popupEvaluation.isClear': false,
+          paddingHeight: '' // 清空值
+        })
+      };
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  /**
+   * @author: yilingsj（315800015@qq.com）
+   * @description: 点击时显示评论组件
+   * @return {void}
+   * @Date: 2020-04-10 21:59:38
+   */
+  handleIsShowComment() {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      'popupEvaluation.isShow': true,
+      'popupEvaluation.id': 0,
+      /* id为动态，请修改 */
+      'popupEvaluation.typeName': 'card',
+      /* typeName也为动态，请修改 */
+      'popupEvaluation.isClear': this.data.popupEvaluation.isClear
     })
+  },
+  /**
+   * @author: yilingsj（315800015@qq.com）
+   * @description: 滚动到页尾
+   * @return {void}
+   * @Date: 2020-06-09 17:11:55
+   */
+  pageScrollBottom() {
+    wx.pageScrollTo({
+      selector: '.evaluation-end'
+    })
+  },
+  /**
+   * @author: yilingsj（315800015@qq.com）
+   * @description: 滚动到最新评论（第一条）的位置
+   * @return {void}
+   * @Date: 2020-06-09 17:12:33
+   */
+  pageScrollFirst() {
+    wx.pageScrollTo({
+      selector: '.evaluation-wrap'
+    })
+  },
+  /**
+   * @author: yilingsj（315800015@qq.com）
+   * @description: 初始化一堆假数据
+   * @return {void}
+   * @Date: 2020-06-09 17:39:40
+   */
+  initData() {
+    let data = []
+    for (let i = 0; i < 20; i++) {
+      data.push({
+        name: '佚名' + (Number(i) + 1),
+        text: '第' + (Number(i) + 1) + '条评论',
+        type: '资讯',
+        time: Date.now()
+      })
+    }
+    this.setData({
+      evaluationData: data
+    })
+  },
+  /**
+   * @author: yilingsj（315800015@qq.com）
+   * @description: 获取dom信息
+   * @param {String} name name
+   * @param {Function} callback 回调
+   * @return {void}
+   * @Date: 2020-02-06 16:44:24
+   */
+  getDom(name, callback) {
+    let query = wx.createSelectorQuery().in(this);
+    query.select(name).boundingClientRect();
+    query.exec(res => {
+      callback && callback(res);
+    });
   }
 })
